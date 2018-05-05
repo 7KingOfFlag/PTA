c7818aef84f9258d9f9127a48432e306f95ff46c
陈永标 <1435069316@qq.com>
2018-05-06 01:37:04 +0800

    add search

--- 7-34.c
+++ 7-34.c
@@ -1,34 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct{
-    char name[12],birthday[10];
-    char six[1];
-    char phoneNumber[12],mobilePhoneNumber[14];
+    char name[11],birthday[11];
+    char six;
+    char phoneNumber[16],mobilePhoneNumber[16];
 }phoneList;
 
 int main(void){
-    int N,i;
+    int N,i,listLenght;
     scanf("%d",&N);
-    printf("your input %d\n",N);
     phoneList *list = (phoneList*)malloc(sizeof(phoneList)*N);
+    listLenght = N;
     for(i=0;i<N;i++){
-        printf("please key phoneList\n");
-        scanf("%s %s %s %s %s",
+        scanf("%s %s %c %s %s",
         list[i].name,list[i].birthday,list[i].six,
         list[i].phoneNumber,list[i].mobilePhoneNumber);
-        printf("list insat done!!\n");
     }
+    scanf("%d",N);
+    int *search = (int*)malloc(sizeof(int)*N);
     for(i=0;i<N;i++){
-        printPhoneList(list[i]);
-        printf("\n-------------------\n");
+        scanf("%d",search[i]);
     }
-    getchar();
+
+    for(i=0;i<N;i++){
+        if(search[i]<listLenght){
+            printPhoneList(list[i]);
+        }else{
+            printf("Not Found");
+        }
+    }
     return 0;
 }
 void printPhoneList(phoneList *list){
-    printf("%s ",list->name);
-    printf("%s ",list->birthday);
-    printf("%s ",list->six);
-    printf("%s ",list->phoneNumber);
-    printf("%s",list->mobilePhoneNumber);
+    printf("1.%s ",list->name);
+    printf("2.%s ",list->birthday);
+    printf("3.%c ",list->six);
+    printf("4.%s ",list->phoneNumber);
+    printf("5.%s",list->mobilePhoneNumber);
 }
