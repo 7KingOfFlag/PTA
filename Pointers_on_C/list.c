680f96b258d7dcf615efba3c76f86395a1423b57
陈永标 <1435069316@qq.com>
2018-06-07 01:01:03 +0800

    完成链表模块：新建链表，初始化链表，插入节点，删除节点，释放链表

--- Pointers_on_C/list.c
+++ Pointers_on_C/list.c
@@ -12,14 +12,61 @@ typedef struct NODE
 	struct NODE *link;
 	int value;
 } Node;
-void printSll(Node *head);
-Node *MakeSll(int sllSize)
-{
-	Node *head, *new_Node, *previous;
-	int i;
-	printf("fist done!\n");
 
-	for (i = 0; i < sllSize; i++)
+void printSll(const Node *root);
+Node *sllMake(int sllSize);
+void sllInitialization(const Node *root, const int *arrary, int arraryLen);
+int sllInsert(const Node *root, int new_value);
+int sllRm(Node *root, int rm_value, int times);
+Node *sllFree(Node *root);
+int main(void)
+{
+	Node *Sll = sllMake(100000);
+	int arrary[] = {2, 5, 6, 7, 9, 15, 20};
+
+	sllInitialization(Sll, arrary, sizeof(arrary) / sizeof(int));
+
+
+	Sll = sllFree(Sll);
+
+	Sll = sllMake(20);
+
+	printSll(Sll);
+
+	return 0;
+}
+
+void printSll(const Node *root)
+{
+	int i;
+	if (root == NULL)
+	{
+		printf("is NULL\n");
+	}
+	else
+	{
+		Node *current;
+		current = root->link;
+		for (i = 0; i < root->value; i++)
+		{
+			printf("%d.%d path = %x\n", i + 1, current->value, current);
+			current = current->link;
+		}
+		printf("--------------------------------------------\n");
+	}
+}
+
+Node *sllMake(int sllSize)
+{
+	/*
+	生产一个链表，返回链表头root
+	root->value 是链表的大小
+	其他节点的值初始化为 0
+	*/
+	Node *root = NULL, *new_Node = NULL, *previous = NULL;
+	int i;
+
+	for (i = 0; i < sllSize + 1; i++)
 	{
 		new_Node = (Node *)malloc(sizeof(Node));
 		if (new_Node == NULL)
@@ -31,81 +78,125 @@ Node *MakeSll(int sllSize)
 		{
 			if (i == 0)
 			{
-				printf("into else\n");
-				head = new_Node;
-				head->value = sllSize;
-				printf("Head node done!!\n Head's value is %d \n", head->value);
+				root = new_Node;
+				root->value = sllSize;
 			}
 			else
 			{
 				previous->link = new_Node;
-				previous->link->value = i;
-				printf("The %d node done !! He's path = %x \nThe %d node's value is %d \n", i, new_Node, i, new_Node->value);
+				previous->link->value = 0;
 			}
 			previous = new_Node;
 		}
 
-		if (i == sllSize - 1)
+		if (i == sllSize)
 		{
 			new_Node->link = NULL;
 		}
 	}
-	return head;
+	return root;
 }
-int sll_insert(Node *current, int new_value)
-{
-	Node *previous;
-	Node *new_Node;
 
-	//寻找插入节点
-	while (current->value < new_value)
+void sllInitialization(const Node *root, const int *arrary, int arraryLen)
+{
+	/*
+	使用数组arrary中的值初始化链表
+	如果链表比数组长则超出的部分链表的值为0
+	*/
+	Node *current = root->link;
+	int i;
+
+	for (i = 0; i < root->value; i++)
 	{
-		previous = current;
+		if (i < arraryLen)
+			current->value = arrary[i];
+		else
+			current->value = 0;
+
+		current = current->link;
+	}
+}
+
+int sllInsert(const Node *root, int new_value)
+{
+	Node *new_Node, *current;
+	current = root->link;
+	//寻找插入节点
+	while (current->value < new_value && current->link != NULL)
+	{
 		current = current->link;
 	}
 
 	//创建新节点并存储新值，如果内存分配失败，函数返回 FALSE
 	new_Node = (Node *)malloc(sizeof(Node));
-
 	if (new_Node == NULL)
 		return FALSE;
-	new_Node->value = new_value;
 
+	new_Node->value = new_value;
 	//插入新节点
-	new_Node->link = current;
-	previous->link = new_Node;
+	new_Node->link = current->link;
+	current->link = new_Node;
 	return TRUE;
 }
 
-int main(void)
+int sllRmINValue(Node *root, int rm_value, int times)
 {
-	Node *fistSll = MakeSll(10);
-	printf("AA\n");
-	int i = 0;
-	if(sll_insert(fistSll,10) == TRUE)
-	{
-		fistSll->value++;
-		printf("insert value %d\n",10);
-		printSll(fistSll);
-	}
-	return 0;
-}
+	/*
+	移除单链表中value为rm_value的节点
+	times 移除次数 ：
+		times > 1 时最多移除 times 次
+		times == 0 时 移除所有
+	*/
 
-void printSll(Node *head){
-	int i;
-	if (head == NULL)
+	if (root->link == NULL)
+		return FALSE;
+
+	Node *current = root->link;
+	Node *previous = root;
+	int flag = 0;
+	while (current != NULL)
 	{
-		printf("is NULL\n");
-	}
-	else
-	{
-		Node *current;
-		current = head;
-		for (i = 0; i < head->value; i++)
+		if (current->value == rm_value)
 		{
-			printf("%d.%d path = %x\n", i, current->value, current);
+			if (times == 0 || flag < times)
+			{
+				previous->link = current->link;
+				free(current);
+				current = previous->link;
+				flag++;
+				root->value = root->value - 1;
+			}
+			else
+				break;
+		}
+		else
+		{
+			previous = current;
 			current = current->link;
 		}
 	}
+	return TRUE;
 }
 
+Node *sllFree(Node *root)
+{
+	/*
+	释放单链表root
+	*/
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	else
+	{
+		Node *current = root->link, *previous = root;
+		
+		while(current != NULL){
+			previous->link = current->link;
+			free(current);
+			current = previous->link;
+		}
+		free(root);
+		return NULL;
+	}
+}
